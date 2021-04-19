6170bdbb3c0385959a4f4310993fbc75ed4ae094
Ben Heard <beheard@microsoft.com>
2021-04-19 11:57:39 -0400

    Added some changes following the lecture and filled out the first quiz

--- Demos/Module2/variables_start.py
+++ Demos/Module2/variables_start.py
@@ -3,11 +3,19 @@
 #
 
 # Declare a variable and initialize it
+person_name = 'Ben Heard'
+person_address = '8628 Windjammer'
+print(f'{person_name} lives at {person_address}')
 
 # Inspect the data type
+print(type(person_name))
 
 # re-declaring the variable works
-
+person_name = 1
+print(person_name)
+print(type(person_name))
+print(f'{person_name} lives at {person_address}')
+print(str(person_name) + " lives at " + person_address)
 
 # ERROR: variables of different types cannot be combined
 #print("this is a string" + 123)
