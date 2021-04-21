361ba2f9c9de92d167bfe701c6b94c29bbf4f3ff
Daniel Bruno <daniel.c.bruno@gmail.com>
2021-04-21 07:41:39 -0400

    Update setup_python_environment.sh

--- Demos/Module7/setup_python_environment.sh
+++ Demos/Module7/setup_python_environment.sh
@@ -2,11 +2,16 @@
 # which is the opposite of the default shell behaviour, which is to ignore errors in scripts.
 set -e
 
-#To create a virtual environment, use the following command, where ".venv" is the name of the environment folder:
+# Install virtualenv globally
+pip install virtualenv
+
+# To create a virtual environment, use the following command, where ".venv" is the name of the environment folder:
 python -m venv .venv
 
 #https://code.visualstudio.com/docs/python/environments
 
-.\.venv\Scripts\activate
+# activate virtual environment within the terminal
+.\.venv\Scripts\activate.ps1
 
+# when you want to write to a file your packages you have installed already so you can re-install them automaticall in another computer.
 python -m pip freeze > requirements.txt
