b02f6c6c12894d8384ac389494ada0311fa0d5cc
Daniel Bruno <daniel.c.bruno@gmail.com>
2021-04-21 08:53:18 -0400

    Update access_modifiers_start.py

--- Demos/Module6/access_modifiers_start.py
+++ Demos/Module6/access_modifiers_start.py
@@ -19,30 +19,10 @@ class Parent:
     def access_private_members(self):     
         self.__display_private_members()
 
-# super class 2
-# class Parent2:
-#     public_variable = None
-#     _protected_variable = None
-#     __private_variable = None
-
-#     def __init__(self, var1, var2, var3):  
-#         self.public_variable = var1
-#         self._protected_variable = var2
-#         self.__private_variable = var3     
-#     def display_public_members(self):
-#         print("Parent 2 Public Data Member: ", self.public_variable)
-#     def _display_protected_members(self):
-#         print("Parent 2 Protected Data Member: ", self._protected_variable)
-#     def __display_private_members(self):
-#         print("Parent 2 Private Data Member: ", self.__private_variable)
-#     def access_private_members(self):     
-#         self.__display_private_members()
-
 # derived class
 class Child(Parent):
     def __init__(self, var1, var2, var3): 
-        Parent.__init__(self, var1, var2, var3)
-        #Parent2.__init__(self, var1, var2, var3)
+        super().__init__(var1, var2, var3)
     def access_protected_members(self):
         self._display_protected_members()
    
